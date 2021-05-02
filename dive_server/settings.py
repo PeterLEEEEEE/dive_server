c6ef4bad5cf783c3ac4874712e85860713a2a6f6
PeterLEEEEEE <dissgogo@naver.com>
2021-05-03 02:17:08 +0900

    allow 8000

--- dive_server/settings.py
+++ dive_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7r1uo_is!*a$(3sh$5eoss#!)w*j@^x7509$$fi97$vku85gp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
